9c2d5ed7b06f3a5cda776d354004dbad215679b8
Denis Shtupa <denis.shtupa@gmail.com>
2020-12-23 14:42:10 +0100

    changes ons package.json and server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// app.use(express.static('./dist'));
-
-// app.get('/*', function(req, res) {
-//   res.sendFile('index.html', {root: 'dist/'}
-// );
-// });
-
-// app.listen(process.env.PORT || 8080);
+// server.js
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
