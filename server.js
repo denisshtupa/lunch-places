9d077eaef9a9a140c343730509e3d3f8bfa256e9
Denis Shtupa <denis.shtupa@gmail.com>
2020-12-23 14:23:02 +0100

    change server.js as in angular

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
+const express = require('express');
+const path = require('path');
 
-const express = require('express')
-const path = require('path')
-const history = require('connect-history-api-fallback')
+const app = express();
 
-const app = express()
+app.use(express.static('./dist'));
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/'}
+);
+});
 
-app.use(staticFileMiddleware)
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}))
-app.use(staticFileMiddleware)
-
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'))
-})
-
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port
-  console.log('App now running on port', port)
-})
+app.listen(process.env.PORT || 8080);
